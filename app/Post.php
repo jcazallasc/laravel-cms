cb5acb200d53e19c74763bbeb614c86e6919b29f
Javier Cazallas <jcazallasc@gmail.com>
2020-05-25 16:09:10 +0200

    Add soft delete for posts

--- app/Post.php
+++ app/Post.php
@@ -3,8 +3,24 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Post extends Model
 {
-    //
+
+    use SoftDeletes;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = ['title', 'description', 'content', 'published_at', 'image'];
+
+    /**
+     * The attributes that aren't mass assignable.
+     *
+     * @var array
+     */
+    protected $guarded = [];
 }
