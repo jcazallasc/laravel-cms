b043071611fb4d1eb81d20aca740477e972b8990
Javier Cazallas <jcazallasc@gmail.com>
2020-05-26 15:04:21 +0200

    Add security to avoid delete used categories/tags

--- app/Http/Controllers/CategoriesController.php
+++ app/Http/Controllers/CategoriesController.php
@@ -79,6 +79,11 @@ class CategoriesController extends Controller
      */
     public function destroy(Category $category)
     {
+        if($category->posts->count() > 0) {
+            Session::flash('danger', 'Category cannot be deleted because it has some posts');
+            return redirect()->back();
+        }
+
         $category->delete();
 
         Session::flash('success', 'Category deleted successfully!');
--- app/Http/Controllers/TagsController.php
+++ app/Http/Controllers/TagsController.php
@@ -79,6 +79,11 @@ class TagsController extends Controller
      */
     public function destroy(Tag $tag)
     {
+        if ($tag->posts->count() > 0) {
+            Session::flash('danger', 'Tag cannot be deleted because it has some posts');
+            return redirect()->back();
+        }
+
         $tag->delete();
 
         Session::flash('success', 'Tag deleted successfully!');
