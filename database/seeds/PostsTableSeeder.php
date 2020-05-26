7bfb2a8cb09966cfc69ce03f179040fc92bc107c
Javier Cazallas <jcazallasc@gmail.com>
2020-05-26 16:22:21 +0200

    Seed with fake data

---
+++ database/seeds/PostsTableSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+use App\Tag;
+use App\Post;
+use App\Category;
+use Illuminate\Database\Seeder;
+
+class PostsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        factory(Category::class, 5)->create();
+        $tags = factory(Tag::class, 5)->create();
+        $posts = factory(Post::class, 10)->create();
+        
+
+        $posts->each(function (Post $post) use ($tags) {
+            $post->tags()->attach(
+                $tags->random(rand(1, 5))->pluck('id')->toArray()
+            );
+            $post->category_id = rand(1, 5);
+            $post->save();
+        });
+    }
+}
