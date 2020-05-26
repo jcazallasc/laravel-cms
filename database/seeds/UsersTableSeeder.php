efa2095b79a6b3465ca011510740078f52383e4d
Javier Cazallas <jcazallasc@gmail.com>
2020-05-26 15:06:06 +0200

    Fill database with an admin and an user

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,35 @@
+<?php
+
+use App\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $user = User::where('email', 'jcazallasc@gmail.com')->first();
+        if (!$user) {
+            User::create([
+                'name' => 'Javier',
+                'email' => 'jcazallasc@gmail.com',
+                'password' => Hash::make('cazallas'),
+                'role' => 'admin',
+            ]);
+        }
+
+        $user = User::where('email', 'test@test.com')->first();
+        if (!$user) {
+            User::create([
+                'name' => 'Test',
+                'email' => 'test@test.com',
+                'password' => Hash::make('test'),
+            ]);
+        }
+    }
+}
