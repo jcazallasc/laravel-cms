75f6ca909f1dbaf1b85843d9c7d5635e89012a8f
Javier Cazallas <jcazallasc@gmail.com>
2020-05-27 14:59:32 +0200

    Improve fake post content

--- database/factories/PostFactory.php
+++ database/factories/PostFactory.php
@@ -3,16 +3,17 @@
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
 
 use App\Post;
-use App\Category;
+use App\User;
 use Faker\Generator as Faker;
 
 $factory->define(Post::class, function (Faker $faker) {
     return [
         'title' => $faker->sentence(3),
         'description' => $faker->sentence(6),
-        'content' => $faker->paragraph(4),
+        'content' => $faker->text(2000),
         'category_id' => 1,
         'published_at' => $faker->dateTime,
         'image' => $faker->imageUrl(),
+        'user_id' => User::where('email', '=', 'jcazallasc@gmail.com')->first(),
     ];
 });
